f6b7b9baa9ada6676136cd125dd30a52709f686c
Sajal Namdeo <sajalnamdeo627@gmail.com>
2025-08-04 04:58:43 +0530

    added render base url

--- config/axiosUrl.ts
+++ config/axiosUrl.ts
@@ -1,4 +1,9 @@
-export const baseUrl = 'https://lendlocal-backend-production.up.railway.app/api';
-export const backendUrl = 'https://lendlocal-backend-production.up.railway.app';
+// export const baseUrl = 'https://lendlocal-backend-production.up.railway.app/api';
+// export const backendUrl = 'https://lendlocal-backend-production.up.railway.app';
+
+export const baseUrl = "https://lendlocal-backend.onrender.com/api";    
+export const backendUrl = "https://lendlocal-backend.onrender.com";
+
+
 // export const baseUrl = 'http://localhost:8080/api'; 
 // export const backendUrl = 'http://localhost:8080';
