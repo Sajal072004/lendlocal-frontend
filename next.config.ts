5ddc4611e51ee416069666211449ed829d720143
sajalnamdeo-stablemoney <sajal.namdeo@stablemoney.in>
2026-05-03 18:06:18 +0530

    feat: add loremflickr.com to allowed image domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['res.cloudinary.com', 'placehold.co', 'picsum.photos', 'fastly.picsum.photos'],
+    domains: ['res.cloudinary.com', 'placehold.co', 'picsum.photos', 'fastly.picsum.photos', 'loremflickr.com'],
   },
 };
 
